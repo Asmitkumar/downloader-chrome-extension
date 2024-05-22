f48928d969f420b7e008df9be3b78541b5c322f1
asmitkumar <asmitkumar11@gmail.com>
2024-05-22 20:21:56 +0530

    Minor changes

--- contentScript.js
+++ contentScript.js
@@ -34,7 +34,8 @@ function handleSiteA() {
     console.log('Sending message:', textContent);
     chrome.storage.local.set({ textContent: textContent });
 
-    // Navigate to the download URL
+    // Navigate to the download/any page URL
+    // Only for demo purpose
     chrome.runtime.sendMessage({ navigate: "https://jsonviewer.stack.hu/" });
 
     // Download file
